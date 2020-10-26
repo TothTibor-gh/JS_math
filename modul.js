7c12f4a8bf04762aee94c9d7ce41347eeb0aa9e1
aniko <s.aniko9424@gmail.com>
2020-10-26 11:46:55 +0100

    Hozzáadtam az oddMult függvényt.

--- modul.js
+++ modul.js
@@ -6,3 +6,13 @@ const contains = (array, num) => {
   }
   return false;
 };
+
+const oddMult = (array) => {
+  let mult = 1;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 === 1) {
+      mult *= array[i];
+    }
+  }
+  return mult;
+};
