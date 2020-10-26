a8fb0c1e49378dde930a35c476baf8154e0c9789
aniko <s.aniko9424@gmail.com>
2020-10-26 11:04:28 +0100

    Hozzáadtam a random szám generálást és a függvények hívását.

---
+++ main.js
@@ -0,0 +1,8 @@
+const { pow } = require('./pow');
+const { fact } = require('./fact');
+
+const a = Math.floor(Math.random() * 8) + 1;
+const b = Math.floor(Math.random() * 8) + 1;
+
+pow(a, b);
+fact(a);
