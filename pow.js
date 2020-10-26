66c32d74efdec30115e9f324b28ceab749a60e41
TTibi1 <tibi30@freemail.hu>
2020-10-26 10:50:11 +0100

    exportalas hozzaadasa

--- pow.js
+++ pow.js
@@ -1,7 +1,3 @@
-// 5. A tulajdonos feladata lesz a főprogram készítése, valamint egy modul készítése, 
-// ami egy metódust tárol, melynek feladata a hatványozás. 
-// (Vár 2 szám paramétert, és iteratívan kiszámítja az a^b számot, és visszatér vele!)
-
 const pow = (szam, hatvanya) => {
     let sum = 1;
     for (let i = 0; i < hatvanya; i++) {
@@ -10,4 +6,6 @@ const pow = (szam, hatvanya) => {
     return sum;
 };
 
-console.log(pow(2, 5));
+module.exports = {
+    pow
+};
